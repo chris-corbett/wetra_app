c90c537aeab6a2ddc4f03170dc045f95650e05a0
Nigam Patel <W0752460@myscc.ca>
2022-03-14 19:36:51 -0400

    Below screens are created: Login screen SignUp screen Screens for bottom-navigation-bar All screens and navigation-bar working properly with title

---
+++ lib/Main_pages/setting_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text("Setting"),
+      centerTitle: true,
+      automaticallyImplyLeading: false,
+    ));
+  }
+}
