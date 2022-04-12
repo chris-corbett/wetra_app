6792283c64d57643834904efe426d6afe7dc2c7d
Chris Corbett <corbett.chirs1915@gmail.com>
2022-04-12 11:13:21 -0400

    #12 Admins can now delete files * Admins can delete files from the file view screen * Added simple file picker to upload file screen

--- lib/pages/upload_file_screen.dart
+++ lib/pages/upload_file_screen.dart
@@ -1,10 +1,38 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:wetra_app/custom_classes/user.dart';
 
 class UploadFileScreen extends StatefulWidget {
   const UploadFileScreen({Key? key}) : super(key: key);
 
   @override
   State<UploadFileScreen> createState() => _UploadFileScreenState();
+}
+
+_pickFile() async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+  if (result != null) {
+    String token = User.getUser().token;
+    File file = File(result.files.single.path!);
+
+    final response = await http.post(
+        Uri.parse('https://wyibulayin.scweb.ca/wetra/api/files'),
+        headers: <String, String>{
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+          'file=@': file.path,
+          'shared_to=': '0',
+        });
+
+    print(response.body);
+  } else {}
 }
 
 class _UploadFileScreenState extends State<UploadFileScreen> {
@@ -20,8 +48,13 @@ class _UploadFileScreenState extends State<UploadFileScreen> {
           centerTitle: true,
           automaticallyImplyLeading: false,
         ),
-        body: const Center(
-          child: Text('Upload a file here'),
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('Choose a file'),
+            onPressed: () {
+              _pickFile();
+            },
+          ),
         ));
   }
 }
--- lib/pages/view_file_screen.dart
+++ lib/pages/view_file_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wetra_app/custom_classes/uploaded_file.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:wetra_app/custom_classes/user.dart';
+import 'package:http/http.dart' as http;
 
 class ViewFileScreen extends StatefulWidget {
   final UploadedFile file;
@@ -11,23 +14,66 @@ class ViewFileScreen extends StatefulWidget {
 }
 
 class _ViewFileScreenState extends State<ViewFileScreen> {
+  _launchUrl() async {
+    String fileUrl = 'https://wyibulayin.scweb.ca';
+    fileUrl += widget.file.fileUrl.replaceAll(' ', '%20');
+
+    if (await canLaunch(fileUrl)) {
+      await launch(fileUrl);
+    } else {
+      throw 'Could not launch $fileUrl';
+    }
+  }
+
+  _deleteFile() async {
+    String token = User.getUser().token;
+    final response = await http.delete(
+      Uri.parse(
+          'https://wyibulayin.scweb.ca/wetra/api/files/${widget.file.id}'),
+      headers: <String, String>{
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
-            onPressed: () => Navigator.of(context).pop(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
           ),
-          title: const Text('View File'),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: Center(
-          child: Text(widget.file.fileName),
-        ));
+        title: const Text('View File'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(widget.file.fileName),
+            ElevatedButton(
+                onPressed: () {
+                  _launchUrl();
+                },
+                child: const Text('View File')),
+            Visibility(
+              child: ElevatedButton(
+                  onPressed: () {
+                    _deleteFile();
+                  },
+                  child: const Text('Delete')),
+              visible: User.getUser().user.isAdmin == 0 ? false : true,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
