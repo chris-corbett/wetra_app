da49d42dd0cac1d64973f93677276d42ad26abe0
Chris Corbett <corbett.chirs1915@gmail.com>
2022-04-14 00:08:58 -0400

    #16 Chat screen ui added

--- lib/pages/chat_detail_screen.dart
+++ lib/pages/chat_detail_screen.dart
@@ -30,7 +30,6 @@ class ChatDetailScreen extends StatelessWidget {
     if (response.statusCode == 201) {
       chatID =
           ChatDetailList.fromJson(jsonDecode(response.body)).userChat.senderId;
-      print("Chat ID is: $chatID");
       return ChatDetailList.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load messages.');
@@ -49,33 +48,91 @@ class ChatDetailScreen extends StatelessWidget {
         title: Text(chat.firstName),
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: FutureBuilder<List<ChatDetail>>(
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<ChatDetail>? data = snapshot.data;
-              return ListView.builder(
-                  itemCount: data?.length,
-                  shrinkWrap: true,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                        color: Colors.deepOrange[200],
-                        child: ListTile(
-                          title: Text(data![index].lineText),
-                          leading: const SizedBox(
-                            width: 50,
-                            height: 50,
-                            // child: Image.network(data[index].background),
-                          ),
-                        ));
-                  });
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            return const CircularProgressIndicator();
-          },
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(30))),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: ListView.builder(
+                        reverse: true,
+                        itemCount: 5,
+                        itemBuilder: (contx, index) {
+                          return const Text('test');
+                        }),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey, width: 1)),
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: TextField(
+                        decoration: InputDecoration.collapsed(
+                            hintText: "Write your message"),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                      icon: Icon(Icons.send,
+                          color: Theme.of(context).primaryColor, size: 35),
+                      onPressed: null),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
+// Center(
+//         child: FutureBuilder<List<ChatDetail>>(
+//           builder: (context, snapshot) {
+//             if (snapshot.hasData) {
+//               List<ChatDetail>? data = snapshot.data;
+//               return ListView.builder(
+//                   itemCount: data?.length,
+//                   shrinkWrap: true,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     return Card(
+//                         color: Colors.deepOrange[200],
+//                         child: ListTile(
+//                           title: Text(data![index].lineText),
+//                           leading: const SizedBox(
+//                             width: 50,
+//                             height: 50,
+//                             // child: Image.network(data[index].background),
+//                           ),
+//                         ));
+//                   });
+//             } else if (snapshot.hasError) {
+//               return Text("${snapshot.error}");
+//             }
+//             return const CircularProgressIndicator();
+//           },
+//         ),
+//       ),
