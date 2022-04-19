3ef9cac78568cdfddee6f54ff0d8daacfde36eff
Chris Corbett <corbett.chirs1915@gmail.com>
2022-04-18 22:02:08 -0400

    Made some changes to the upload file screen

--- lib/pages/upload_file_screen.dart
+++ lib/pages/upload_file_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:wetra_app/custom_classes/api_const.dart';
 import 'package:wetra_app/custom_classes/group.dart';
+import 'package:wetra_app/custom_classes/login_user.dart';
 import 'package:wetra_app/custom_classes/user.dart';
 
 class UploadFileScreen extends StatefulWidget {
@@ -14,15 +15,31 @@ class UploadFileScreen extends StatefulWidget {
   State<UploadFileScreen> createState() => _UploadFileScreenState();
 }
 
+List<LoginUser> users = [
+  const LoginUser(
+      id: 0,
+      firstName: 'All',
+      lastName: 'Users',
+      email: '',
+      createdAt: '',
+      updatedAt: '',
+      registeredDate: '')
+];
+List<Group> groups = [
+  const Group(id: 0, name: 'All Groups', createdAt: '', updatadAt: '')
+];
+Group groupDropdownValue = groups.first;
+LoginUser userDropdownValue = users.first;
+
 class _UploadFileScreenState extends State<UploadFileScreen> {
   final TextEditingController descriptionController = TextEditingController();
 
   String fileName = 'No File Selected';
-  late Group groupDropdownValue;
+
+  bool shareIsChecked = false;
 
   @override
   void initState() {
-    getGroups();
     super.initState();
   }
 
@@ -32,37 +49,33 @@ class _UploadFileScreenState extends State<UploadFileScreen> {
     super.dispose();
   }
 
-  _pickFile() async {
+  Future<File> _pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
 
     if (result != null) {
-      String token = User.getUser().token;
       File file = File(result.files.single.path!);
 
       setState(() {
         fileName = file.uri.pathSegments.last;
       });
 
-      // final response = await http.post(
-      //   Uri.parse('https://wyibulayin.scweb.ca/wetra/api/files'),
-      //   headers: <String, String>{
-      //     'Accept': 'application/json',
-      //     'Authorization': 'Bearer $token',
-      //     'Content-Type': 'multipart/form-data',
-      //   },
-      //   body: jsonEncode({'file=@': file.path, 'shared_to=': '0'}),
-      // );
+      return file;
+    } else {
+      return File('');
+    }
+  }
 
-      // print(file.path);
-      // final request =
-      //     http.MultipartRequest('POST', Uri.parse(ApiConst.api + 'files'));
-      // request.fields['group_id'] = '0';
-      // request.files.add(http.MultipartFile.fromString('file', file.path));
-      // request.send().then((response) {
-      //   print(response.statusCode);
-      // });
-      //print(response.body);
-    } else {}
+  uploadFile() {
+    // String token = User.getUser().token;
+    // File file = File(result.files.single.path!);
+
+    // final request =
+    //     http.MultipartRequest('POST', Uri.parse(ApiConst.api + 'files'));
+    // request.fields['group_id'] = '0';
+    // request.files.add(http.MultipartFile.fromString('file', file.path));
+    // request.send().then((response) {
+    //   print(response.statusCode);
+    // });
   }
 
   Future<List<Group>> getGroups() async {
@@ -73,9 +86,22 @@ class _UploadFileScreenState extends State<UploadFileScreen> {
       'Authorization': 'Bearer $token',
     });
 
-    List<Group> groups = FullGroup.fromJson(jsonDecode(response.body)).groups;
-    groupDropdownValue = groups.first;
-    return groups;
+    return FullGroup.fromJson(jsonDecode(response.body)).groups;
+  }
+
+  Future<List<LoginUser>> getFullUsers() async {
+    String token = User.getUser().token;
+    final response = await http.get(
+      Uri.parse(ApiConst.api + 'users'),
+      headers: <String, String>{
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    List<LoginUser> users =
+        GetFullUser.fromJson(jsonDecode(response.body)).users;
+    return users;
   }
 
   @override
@@ -117,47 +143,107 @@ class _UploadFileScreenState extends State<UploadFileScreen> {
                     labelText: 'File Description',
                   )),
             ),
-            FutureBuilder<List<Group>>(
-              future: getGroups(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: Text('Loading groups'));
-                } else {
-                  if (snapshot.hasError) {
-                    return const Center(child: Text('Error loading groups'));
-                  } else {
-                    List<Group> groups = snapshot.data!;
-                    groups.insert(
-                        0,
-                        const Group(
-                            id: 0,
-                            name: 'All Groups',
-                            createdAt: '',
-                            updatadAt: ''));
-
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text('Status:'),
-                        const SizedBox(width: 15),
-                        DropdownButton<Group>(
-                          value: groupDropdownValue,
-                          items: snapshot.data
-                              ?.map((group) => DropdownMenuItem(
-                                  child: Text(group.name), value: group))
-                              .toList(),
-                          onChanged: (Group? newValue) {
-                            setState(() {
-                              groupDropdownValue = newValue!;
-                            });
-                          },
-                        ),
-                      ],
-                    );
-                  }
-                }
+            CheckboxListTile(
+              title: const Text('Share to group'),
+              value: shareIsChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  shareIsChecked = value!;
+                });
               },
+            ),
+            Visibility(
+              visible: shareIsChecked,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    const Text('Group:'),
+                    const SizedBox(width: 15),
+                    FutureBuilder<List<Group>>(
+                      future: getGroups(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(child: Text('Loading groups'));
+                        } else {
+                          if (snapshot.hasError) {
+                            return const Center(
+                                child: Text('Error loading groups'));
+                          } else {
+                            if (groups.length == 1) {
+                              groups.addAll(snapshot.data!);
+                            }
+
+                            return DropdownButton<Group>(
+                              value: groupDropdownValue,
+                              items: groups
+                                  .map<DropdownMenuItem<Group>>((Group group) {
+                                return DropdownMenuItem<Group>(
+                                  value: group,
+                                  child: Text(group.name),
+                                );
+                              }).toList(),
+                              onChanged: (Group? newValue) {
+                                setState(() {
+                                  groupDropdownValue = newValue!;
+                                });
+                              },
+                            );
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Visibility(
+              visible: !shareIsChecked,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    const Text('User:'),
+                    const SizedBox(width: 15),
+                    FutureBuilder<List<LoginUser>>(
+                      future: getFullUsers(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(child: Text('Loading users'));
+                        } else {
+                          if (snapshot.hasError) {
+                            return const Center(
+                                child: Text('Error loading users'));
+                          } else {
+                            if (users.length == 1) {
+                              users.addAll(snapshot.data!);
+                            }
+
+                            return DropdownButton<LoginUser>(
+                              value: userDropdownValue,
+                              items: users.map<DropdownMenuItem<LoginUser>>(
+                                  (LoginUser user) {
+                                return DropdownMenuItem<LoginUser>(
+                                  value: user,
+                                  child: Text(
+                                      user.firstName + ' ' + user.lastName),
+                                );
+                              }).toList(),
+                              onChanged: (LoginUser? newValue) {
+                                setState(() {
+                                  userDropdownValue = newValue!;
+                                });
+                              },
+                            );
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ));
